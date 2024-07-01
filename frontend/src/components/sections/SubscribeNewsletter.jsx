bd56e3e0049c923e5ade9d59553427c30fdb9bac
PrateekTechmatys <prateeky562@gmail.com>
2024-07-01 11:34:30 +0530

    favicon

--- frontend/src/components/sections/SubscribeNewsletter.jsx
+++ frontend/src/components/sections/SubscribeNewsletter.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import '../../css/Subscribe.css'
 
 function SubscribeNewsletter() {
   const [inputEmail, setInputEmail] = useState("");

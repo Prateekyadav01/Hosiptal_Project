0c93e0e725727a347a0cd24dde6e96ab789c54eb
PrateekTechmatys <prateeky562@gmail.com>
2024-07-01 10:20:29 +0530

    making responsive

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -6,6 +6,8 @@ import Section1 from './sections/Section1'
 import Section2 from './sections/Section2'
 import Footer from './sections/Footer'
 import SliderComponent from './sections/Slider'
+import Info from './sections/SectionNew1'
+import About from './sections/About'
 // import Slider from 'react-slick'
 
 const Layout = () => {
@@ -14,7 +16,9 @@ const Layout = () => {
       <Navbar/>
       <Hero/>
       {/* <SliderComponent/> */}
-      <Section1/>
+      <Info/>  
+      {/* <Section1/> */}
+      <About/>
       <Section2/>
       <Footer/>
       {/* <Home/> */}

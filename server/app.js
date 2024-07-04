4543c5dde6eed6e908de6bab1aa7cf51a8c14f34
PrateekTechmatys <prateeky562@gmail.com>
2024-07-04 18:26:19 +0530

    cors erroir is removed

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN ||  'http://localhost:5173',
+    origin: [process.env.CORS_ORIGIN ,  'http://localhost:5173'],
     credentials: true
 }))
 
